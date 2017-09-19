af329f419ccae8f12b0f021f4488720de23fe69c
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-19 22:11:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var recipes = Object()
 
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object
+  var newob = Object();
+  newob.assign(object);
+  newob[key] = value;
+  return newob
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
