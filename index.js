4ce5d70e86d03b4c8ae8d11a0e35986aaf629383
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-19 22:03:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 var recipes = Object()
+
+
+
+
+function updateObjectWithKeyAndValue(object, key, value){}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){}
+
+function deleteFromObjectByKey(object, key){}
+
+function destructivelyDeleteFromObjectByKey(object, key){}
