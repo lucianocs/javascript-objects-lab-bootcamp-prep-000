67d1c77a79bcfd2dc653b28745eb7b7cb074945d
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-19 22:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ var recipes = Object()
 
 
 
-function updateObjectWithKeyAndValue(object, key, value){}
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+}
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+}
 
-function deleteFromObjectByKey(object, key){}
+function deleteFromObjectByKey(object, key){
+  delete object[key]
+}
 
-function destructivelyDeleteFromObjectByKey(object, key){}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+}
